b95da1a3d90d4d4b84a6188271384fd554fcfb13
Locutus <edupagotto@gmmail.com>
2015-02-01 17:21:03 -0200

    Equalizada fonte win32-Posix

--- chimera/src/GameClient.cpp
+++ chimera/src/GameClient.cpp
@@ -1,10 +1,4 @@
 #include "GameClient.h"
-
-//#include <SDL2/SDL.h>
-
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-
 #include "Singleton.h"
 
 namespace Chimera {
@@ -18,7 +12,7 @@ GameClient::GameClient ( Video *_pVideo, Chimera::SceneMng *_pScenMng ) : pScene
 #ifdef WIN32
     pFont = new Chimera::Font("C:\\Projetos\\ChimeraEngine\\fonts\\FreeSans.ttf", 18);
 #else
-    pFont = new Chimera::Font ( "/usr/share/fonts/gnu-free/FreeSans.ttf",12 );
+    pFont = new Chimera::Font ( "/home/locutus/Projetos/ChimeraEngine/fonts/FreeSans.ttf",18 );
 #endif
     
     pHUD->addFont ( pFont );
@@ -65,7 +59,7 @@ void GameClient::countFrame() {
 
 void GameClient::open() {
 
-	pVideo->initOpenGL();
+    pVideo->initOpenGL();
     //std::string l_msg =  "OpenGL iniciado com sucesso, versao: " + pSceneMng->getVersaoOpenGL();
     //LOG4CXX_INFO ( logger , l_msg );
 

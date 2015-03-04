75fb3a2f395b019f161448d4105e062e1f9c5b0f
eduardo.pagotto <edupagotto@gmail.com>
2015-03-04 18:17:42 -0300

    teste billboard

--- chimera/src/ParticleEmitter.cpp
+++ chimera/src/ParticleEmitter.cpp
@@ -70,6 +70,32 @@ namespace Chimera {
 			//preserva a cor original
 			glPushAttrib(GL_CURRENT_BIT);
 
+
+
+// 	  float modelview[16];
+// 	  int i,j;
+//
+// 	  // save the current modelview matrix
+// 	  glPushMatrix();
+//
+// 	  // get the current modelview matrix
+// 	  glGetFloatv(GL_MODELVIEW_MATRIX , modelview);
+//
+// 	  // undo all rotations
+// 	  // beware all scaling is lost as well
+// 	  for( i=0; i<3; i++ )
+// 	  for( j=0; j<3; j++ ) {
+// 	   if ( i==j )
+// 	   modelview[i*4+j] = 1.0;
+// 	   else
+// 	   modelview[i*4+j] = 0.0;
+// 	 }
+//
+// 	  // set the modelview with no rotations
+// 	  glLoadMatrixf(modelview);
+
+
+
 			setGL();
 
  			render();
@@ -84,6 +110,8 @@ namespace Chimera {
 // 			Node::update(_dataMsg);
 //
  			glPopMatrix();
+
+// 	  glPopMatrix();
 
 		} else if (_dataMsg->getKindOp() == KindOp::IS_ALLOW_COLLIDE) {
 

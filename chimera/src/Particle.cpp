c1033694c6bebcd37e42a20b7960a370480b3183
eduardo.pagotto <edupagotto@gmail.com>
2015-03-11 16:21:56 -0300

    Correcao do tamanho dos billboards e usode btvector

--- chimera/src/Particle.cpp
+++ chimera/src/Particle.cpp
@@ -40,72 +40,57 @@ namespace Chimera {
 
 	}
 
-
-	static void camera_directions(
-		float * out_rgt,		                                    // Any can be NULL
-		float * out_up,
-		float * out_look)
+// Roughly speaking, a modelview matrix is made up more or less like this:
+// [ EyeX_x EyeX_y EyeX_z    a
+//   EyeY_x EyeY_y EyeY_z    b
+//   EyeZ_x EyeZ_y EyeZ_z    c
+//   um don't look down here ]
+// where a, b, c are translations in _eye_ space.  (For this reason, a,b,c is not
+// the camera location - sorry!)
+	static void camera_directions(btVector3 *right, btVector3 *up, btVector3 *look)
 	{
 		float m[16];
 		glGetFloatv(GL_MODELVIEW_MATRIX, m);
 
-		// Roughly speaking, a modelview matrix is made up more or less like this:
-		// [ EyeX_x EyeX_y EyeX_z    a
-		//   EyeY_x EyeY_y EyeY_z    b
-		//   EyeZ_x EyeZ_y EyeZ_z    c
-		//   um don't look down here ]
-		// where a, b, c are translations in _eye_ space.  (For this reason, a,b,c is not
-		// the camera location - sorry!)
+		if (right != nullptr)
+			right->setValue( m[0], m[4], m[8]);
 
-		if (out_rgt) {
-			out_rgt[0] = m[0];
-			out_rgt[1] = m[4];
-			out_rgt[2] = m[8];
-		}
-		if (out_up) {
-			out_up[0] = m[1];
-			out_up[1] = m[5];
-			out_up[2] = m[9];
-		}
-		if (out_look) {
-			out_up[0] = m[2];
-			out_up[1] = m[6];
-			out_up[2] = m[10];
-		}
+		if (up !=  nullptr)
+			up->setValue( m[1], m[5], m[9]);
+
+		if (look != nullptr)
+			look->setValue( m[2], m[6], m[10]);
+
 	}
 
-	static void draw_billboard(float x, float y, float z)
+	static void draw_billboard(const btVector3 &posicao)
 	{
 
-		float r[3], u[3];
-		camera_directions(r, u, NULL);
+		btVector3 right;
+		btVector3 up;
+
+		camera_directions(&right, &up, nullptr);
 		glBegin(GL_TRIANGLE_STRIP);
 
-		float xf, yf, zf = 0.0;
+		float ks = 10.0f;
+		right /= ks;
+		up /= ks;
 
-		xf = x + r[0] + u[0];
-		yf = y + r[1] + u[1];
-		zf = z + r[2] + u[2];
+	    btVector3 final = posicao + right + up;          // right-up
 		glTexCoord2d(1, 1);
-		glVertex3f(x + r[0] + u[0], y + r[1] + u[1], z + r[2] + u[2]);
+	    glVertex3fv(final.m_floats);                           //   final.x(), final.y(),  final.z());
 
-		xf = x - r[0] + u[0];
-		yf = y - r[1] + u[1];
-		zf = z - r[2] + u[2];
+		final = posicao - right + up;                     // Left-up
 		glTexCoord2d(0, 1);
-		glVertex3f(x - r[0] + u[0], y - r[1] + u[1], z - r[2] + u[2]);
+		glVertex3fv(final.m_floats);
 
-		xf = x + r[0] - u[0];
-		yf = y + r[1] - u[1];
-		zf = z + r[2] - u[2];
+		final = posicao + right - up;                      // right-bottom
 		glTexCoord2d(1, 0);
-		glVertex3f(x + r[0] - u[0], y + r[1] - u[1], z + r[2] - u[2]);
+		glVertex3fv(final.m_floats);
 
-		xf = x - r[0] - u[0];
-		yf = y - r[1] - u[1];
-		zf = z - r[2] - u[2];
+		final = posicao -right - up;                       // left-bottom
 		glTexCoord2d(0, 0);
-		glVertex3f(x - r[0] - u[0], y - r[1] - u[1], z - r[2] - u[2]);
+		glVertex3fv(final.m_floats);
 
 		glEnd();
 	}
@@ -119,10 +104,10 @@ namespace Chimera {
 	void Particle::render() {
 
 		GLuint col = 0;        	// Current Color Selection
-		float slowdown = 2.0f; 	// Slow Down Particles                                
-		float zoom = -40.0f;	// Used To Zoom Out                                   
-		float xspeed = 0;		// Base X Speed (To Allow Keyboard Direction Of Tail) 
-		float yspeed = 0;		// Base Y Speed (To Allow Keyboard Direction Of Tail) 
+		float slowdown = 2.0f; 	// Slow Down Particles
+		float zoom = -40.0f;	// Used To Zoom Out
+		float xspeed = 0;		// Base X Speed (To Allow Keyboard Direction Of Tail)
+		float yspeed = 0;		// Base Y Speed (To Allow Keyboard Direction Of Tail)
 
 		if (active)
 		{
@@ -130,18 +115,18 @@ namespace Chimera {
 			// Fade The Particle Based On It's Life
 			glColor4f(corAtiva.r, corAtiva.g, corAtiva.b, life);//life in alpha
 
-			draw_billboard(position.getX(), position.getY(), position.getZ());
+			draw_billboard(position);
 
-			// Move On The X Axis By X,Y,Z Speed 
+			// Move On The X Axis By X,Y,Z Speed
 			position += direction * (1 / (slowdown * 1000));
 
-			// Take Pull On X,Y,Z Axis Into Account 
+			// Take Pull On X,Y,Z Axis Into Account
 			direction += gravity;
 
-			// Reduce Particles Life By 'Fade' 
+			// Reduce Particles Life By 'Fade'
 			life -= corAtiva.a;
 
-			// If the particle dies, revive it 
+			// If the particle dies, revive it
 			if (life < 0.0f)
 			{
 				btVector3 novo(xspeed + (float)((rand() % 60) - 32.0f),
@@ -226,3 +211,4 @@ namespace Chimera {
 	//	}
 	//}
 }
+

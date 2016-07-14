0b5fe61c484b92cbd53545035fd75f5bf2ca59ef
Locutus <edupagotto@gmail.com>
2016-07-13 22:42:25 -0300

    remocao dr DrawBox e DrawGrid

--- chimera/include/DrawBox.h
+++ chimera/include/DrawBox.h
@@ -1,30 +1,30 @@
 #ifndef DRAW_BOX_H_
 #define DRAW_BOX_H_
 
-#include "Draw.h"
-
-namespace Chimera {
-
-class DrawBox : public Draw
-{
-public:
-    DrawBox (Node *_parent, std::string _name );
-    DrawBox ( const DrawBox &_draw );
-    virtual ~DrawBox();
-
-    void setSizeBox ( const btVector3 &_sizeBox ) {
-        sizeBox = _sizeBox;
-    }
-
-    virtual void init();
-
-    virtual void renderExecute ( bool _texture );
-    virtual btVector3 getSizeBox ( void );
-
-private:
-    btVector3 sizeBox;
-
-};
-}
+//#include "Draw.h"
+//
+//namespace Chimera {
+//
+//class DrawBox : public Draw
+//{
+//public:
+//    DrawBox (Node *_parent, std::string _name );
+//    DrawBox ( const DrawBox &_draw );
+//    virtual ~DrawBox();
+//
+//    void setSizeBox ( const btVector3 &_sizeBox ) {
+//        sizeBox = _sizeBox;
+//    }
+//
+//    virtual void init();
+//
+//    virtual void renderExecute ( bool _texture );
+//    virtual btVector3 getSizeBox ( void );
+//
+//private:
+//    btVector3 sizeBox;
+//
+//};
+//}
 
 #endif
--- chimera/include/DrawGrid.h
+++ chimera/include/DrawGrid.h
@@ -1,55 +1,55 @@
-#ifndef DRAW_GRID_H_
-#define DRAW_GRID_H_
-
-#include "Color.h"
-#include "Draw.h"
-
-namespace Chimera {
-    
-class DrawGrid : public Draw
-{
-public:
-    DrawGrid (Node *_parent, std::string _name );
-    DrawGrid ( const DrawGrid &_draw );
-    virtual ~DrawGrid();
-
-    void setSizeBox ( const btVector3 &_sizeBox ) {
-        sizeBox = _sizeBox;
-    }
-
-    void setSpace ( btScalar _space ) {
-        space = _space;
-    }
-
-    void setColorX ( Color _color ) {
-        colorX = _color;
-    }
-
-    void setColorY ( Color _color ) {
-        colorY = _color;
-    }
-
-    void setColorZ ( Color _color ) {
-        colorZ = _color;
-    }
-
-    //virtual void update ( DataMsg *dataMsg );
-
-    //virtual void clone ( Draw **ppNode );
-
-    virtual void renderExecute ( bool _texture );
-    virtual btVector3 getSizeBox ( void );
-
-
-private:
-    btVector3 sizeBox;
-    btScalar space;
-
-    Color colorX;
-    Color colorY;
-    Color colorZ;
-
-};
-}
-#endif
-// kate: indent-mode cstyle; indent-width 4; replace-tabs on;
+//#ifndef DRAW_GRID_H_
+//#define DRAW_GRID_H_
+//
+//#include "Color.h"
+//#include "Draw.h"
+//
+//namespace Chimera {
+//    
+//class DrawGrid : public Draw
+//{
+//public:
+//    DrawGrid (Node *_parent, std::string _name );
+//    DrawGrid ( const DrawGrid &_draw );
+//    virtual ~DrawGrid();
+//
+//    void setSizeBox ( const btVector3 &_sizeBox ) {
+//        sizeBox = _sizeBox;
+//    }
+//
+//    void setSpace ( btScalar _space ) {
+//        space = _space;
+//    }
+//
+//    void setColorX ( Color _color ) {
+//        colorX = _color;
+//    }
+//
+//    void setColorY ( Color _color ) {
+//        colorY = _color;
+//    }
+//
+//    void setColorZ ( Color _color ) {
+//        colorZ = _color;
+//    }
+//
+//    //virtual void update ( DataMsg *dataMsg );
+//
+//    //virtual void clone ( Draw **ppNode );
+//
+//    virtual void renderExecute ( bool _texture );
+//    virtual btVector3 getSizeBox ( void );
+//
+//
+//private:
+//    btVector3 sizeBox;
+//    btScalar space;
+//
+//    Color colorX;
+//    Color colorY;
+//    Color colorZ;
+//
+//};
+//}
+//#endif
+//// kate: indent-mode cstyle; indent-width 4; replace-tabs on;

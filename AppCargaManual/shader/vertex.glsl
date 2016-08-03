0eaff2e467259e9941a8004b6df50a3fc411f643
Eduardo Pagotto <edupagotto@gmail.com>
2016-08-03 17:13:56 -0300

    Normal Matrix

--- AppCargaManual/shader/vertex.glsl
+++ AppCargaManual/shader/vertex.glsl
@@ -3,17 +3,27 @@ attribute vec4 color;
 attribute vec2 uv1;
 attribute vec3 normal;
 
+//out vec4 eyeCordFs;
+//out vec4 eyeNormalFs;
+
 varying vec2 v_texcoord0;
 varying vec4 v_color;
 varying vec3 v_normal;
 
 uniform mat4 umvMat;
 uniform mat4 upMat;
+uniform mat3 noMat;
 
 void main () {
 
         gl_Position = upMat * umvMat * vec4(vertex, 1.0);
 
+		//vec4 eyeNorm = normalize(noMat * vec4(normal, 0.0));
+		//vec4 eyeCord= modelView * vec4(vertex, 1.0);
+
+		//eyeCordFs = eyeCord;
+		//eyeNormalFs = eyeNorm;
+		
 		v_color = color;
 		v_normal = normal;
 		v_texcoord0 = uv1;
--- chimera/src/Group.cpp
+++ chimera/src/Group.cpp
@@ -3,6 +3,11 @@
 
 #include "OpenGLDefs.h"
 
+//#include <glm\gtx\inverse_transpose.hpp>
+
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_inverse.hpp> 
+
 namespace Chimera {
 
 Group::Group(Node* _parent, std::string _name) : Node (_parent, EntityKind::GROUP, _name) { 
@@ -30,7 +35,8 @@ GLuint Group::applyIdProgram() {
         // Get the variables from the shader to which data will be passed
         GLint mvloc = glGetUniformLocation(idProgram, "umvMat");
         GLint ploc = glGetUniformLocation(idProgram, "upMat");
-    
+        GLint nloc = glGetUniformLocation(idProgram, "noMat");
+        
         // Pass the model-view matrix to the shader
         GLfloat mvMat[16]; 
         glGetFloatv(GL_MODELVIEW_MATRIX, mvMat); 
@@ -40,6 +46,18 @@ GLuint Group::applyIdProgram() {
         GLfloat pMat[16]; 
         glGetFloatv(GL_PROJECTION_MATRIX, pMat); 
         glUniformMatrix4fv(ploc, 1, false, pMat);
+        
+        //glm::mat4 modelViewMat;
+        //float *ptrMat = glm::value_ptr(modelViewMat);
+        //glGetFloatv(GL_MODELVIEW_MATRIX, ptrMat);
+        
+        if (nloc >= 0) {
+            glm::mat4 modelViewMat = glm::make_mat4(mvMat);
+            glm::mat3 gl_NormalMatrix = glm::inverseTranspose(glm::mat3(modelViewMat));
+            float *pnMat = glm::value_ptr(gl_NormalMatrix);
+            glUniformMatrix3fv(nloc, 1, false, pnMat);
+        }
+        
         
     }
     

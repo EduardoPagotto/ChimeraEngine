317a1bd7d1ae49e0c56d370788a63c3ce67e8e63
edupagotto <edupagotto@gmail.com>
2018-11-07 15:27:37 -0200

    optimizacao parse XML

--- chimera_loaders/src/LibraryLights.cpp
+++ chimera_loaders/src/LibraryLights.cpp
@@ -12,33 +12,22 @@ LibraryLights::~LibraryLights() {
 
 Chimera::Light *LibraryLights::target() {
 
-    tinyxml2::XMLElement* l_nLib = root->FirstChildElement("library_lights");
-    if ( l_nLib != nullptr ) {
+    tinyxml2::XMLElement* l_nLight = root->FirstChildElement("library_lights")->FirstChildElement ("light");
+    for (l_nLight; l_nLight; l_nLight = l_nLight->NextSiblingElement()) {
 
-        tinyxml2::XMLElement* l_nNodeBase = l_nLib->FirstChildElement ("light");
-        if ( l_nNodeBase != nullptr ) {
+        std::string l_id = l_nLight->Attribute ( "id" );
+        if (url.compare(l_id) == 0) {
 
-            while ( l_nNodeBase != nullptr ) {
-                std::string l_id = l_nNodeBase->Attribute ( "id" );
-                if (url.compare(l_id) == 0) {
+            Chimera::Light *pLight = new Chimera::Light (nullptr,l_id );
 
-                    Chimera::Light *pLight = new Chimera::Light (nullptr,l_id );
-
-                    auto ret_data = loadDiffuseLightColor(l_nNodeBase);
-                    pLight->setDiffuse(std::get<0>(ret_data));
-                    pLight->setType(std::get<1>(ret_data));
-
-                    return pLight;      
-                }
-                l_nNodeBase = l_nNodeBase->NextSiblingElement();
-            }
-            throw Chimera::ExceptionChimera ( Chimera::ExceptionCode::READ, "Falha, nao encontrado Node: " + std::string ( url ) );
-        } else {
-            throw Chimera::ExceptionChimera ( Chimera::ExceptionCode::READ, "Falha, nao encontrado Node Tipo: " + std::string ( "light" ) );
+            auto ret_data = loadDiffuseLightColor(l_nLight);
+            pLight->setDiffuse(std::get<0>(ret_data));
+            pLight->setType(std::get<1>(ret_data));
+            return pLight;      
         }
-    } else {
-        throw Chimera::ExceptionChimera ( Chimera::ExceptionCode::READ, "Falha, nao encontrado Library: " + std::string ( "library_lights" ) );
     }
+
+    throw Chimera::ExceptionChimera ( Chimera::ExceptionCode::READ, "Light, nao encontrada: " + url);
 }
 
 std::tuple<Chimera::Color, Chimera::LightType> LibraryLights::loadDiffuseLightColor(tinyxml2::XMLElement* _nNode)
@@ -60,7 +49,6 @@ std::tuple<Chimera::Color, Chimera::LightType> LibraryLights::loadDiffuseLightCo
         std::vector<float> l_arrayF;
         const char *l_val = l_nPoint->FirstChildElement ( "color" )->GetText();
         Chimera::loadArrayBtScalar ( l_val, l_arrayF );
-
         Chimera::Color cor(l_arrayF[0], l_arrayF[1], l_arrayF[2], 1.0f);
 
         return std::make_tuple(cor, Chimera::LightType::DIRECTIONAL);
--- chimera_loaders/src/Scene.cpp
+++ chimera_loaders/src/Scene.cpp
@@ -15,52 +15,27 @@ Scene::~Scene() {
 
 Chimera::Node *Scene::target() {
 
+    tinyxml2::XMLElement* child = root->FirstChildElement("scene")->FirstChildElement();
+    for(child;  child;  child = child->NextSiblingElement()) {
 
-    for(tinyxml2::XMLElement* child = root->FirstChildElement("scene")->FirstChildElement();  child != nullptr;  child = child->NextSiblingElement()) {
-
-        const char* val = child->Value();
+        const char* valElemet = child->Value();
 
         if (child->ToElement()->Attribute("url")) {
 
             std::string l_url = child->ToElement()->Attribute("url");
 
-            if (strcmp(val, (const char*)"instance_visual_scene") == 0) {
+            if (strcmp(valElemet, (const char*)"instance_visual_scene") == 0) {
+
                 LibraryVisualScenes lib(root, l_url);
                 pGroup = lib.target();
-            } else if (strcmp(val, (const char*)"instance_physics_scene") == 0) {
+                
+            } else if (strcmp(valElemet, (const char*)"instance_physics_scene") == 0) {
+                
                 LibraryPhysicsScenes lib(root, l_url, pGroup);
                 pPhysicsControl = lib.target();
             }
         }
-            
-    } 
-
-
-    // tinyxml2::XMLElement* l_nLib = root->FirstChildElement("scene");
-    // if ( l_nLib != nullptr ) {
-
-    //     //Chimera::Group *vs = nullptr;
-
-    //     tinyxml2::XMLElement* l_nNodeBase = l_nLib->FirstChildElement ("instance_visual_scene");
-    //     if ( l_nNodeBase != nullptr ) {
-    //         std::string l_url = l_nNodeBase->Attribute ( "url" );
-
-    //         LibraryVisualScenes lib(root, l_url);
-    //         pGroup = lib.target();
-    //     }
-
-    //     l_nNodeBase = l_nLib->FirstChildElement ("instance_physics_scene");
-    //     if ( l_nNodeBase != nullptr ) {
-    //         std::string l_url = l_nNodeBase->Attribute ( "url" );
-
-    //         LibraryPhysicsScenes lib(root, l_url, pGroup);
-    //         pPhysicsControl = lib.target();
-    //     }
-
-        return pGroup;
-
-    // } else {
-    //     throw Chimera::ExceptionChimera ( Chimera::ExceptionCode::READ, "Falha, nao encontrado Library: " + std::string ( "scene" ) );
-    // }
+    }
+    return pGroup;
 }
 }

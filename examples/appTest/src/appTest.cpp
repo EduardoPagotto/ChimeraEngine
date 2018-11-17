607c06ad78ff348b9fd6551906050becfddce694
Eduardo Pagotto <edupagotto@gmail.com>
2018-11-17 17:37:19 -0200

    desenho falho

--- examples/appTest/src/appTest.cpp
+++ examples/appTest/src/appTest.cpp
@@ -53,20 +53,6 @@ int _tmain ( int argc, _TCHAR* argv[] ) {
 
         Video *video = new VideoDevice (canvas["w"].as<int>(), canvas["h"].as<int>(), screen["name"].as<std::string>());
 
-        SceneMng *sceneMng = new SceneMng ( );
-        //sceneMng->setReader(pLoader);
-
-        CameraSpherical *pCam = new CameraSpherical("Observador-01");
-        pCam->setDistanciaMaxima(1000.0);
-        pCam->setDistanciaMinima(0.5);
-        pCam->setFar(10000.0);
-        pCam->setNear(0.1);
-        pCam->setFov(45.0);
-        pCam->setPositionRotation( glm::vec3(300 , 0 ,0) , glm::vec3(0,0,0) );
-        pCam->setPerspective(true);
-        pCam->init();
-        sceneMng->addChild(pCam);
-
         //Carga dos shaders
 		YAML::Node shaders = config["shaders"];
         Chimera::ShadersManager *shader_engine =  Chimera::Singleton<Chimera::ShadersManager>::getRefSingleton();
@@ -78,21 +64,37 @@ int _tmain ( int argc, _TCHAR* argv[] ) {
                                 shader_item["fragment"].as<std::string>());
         }
 
-        Group* group1 = new Group(nullptr, "modelos");
-        group1->setShaderName("debugDepthQuad");
+
+        SceneMng *sceneMng = new SceneMng ( );
+
+        Group* group1 = new Group(sceneMng, "modelos");
+        group1->setShaderName("mesh-default");
+		//group1->setShadowMap(new Chimera::ShadowMapVisitor("shadow1",2048,2048));
+
 
         Transform* pTrans = new Transform(group1,"trans01");
         pTrans->setPosition( glm::vec3( 0.0, 0.0, 0.0) );
+        sceneMng->origemDesenho((Coord*)pTrans);
+
+        CameraSpherical *pCam = new CameraSpherical("Observador-01");
+        pCam->setDistanciaMaxima(1000.0);
+        pCam->setDistanciaMinima(0.5);
+        pCam->setFar(10000.0);
+        pCam->setNear(0.1);
+        pCam->setFov(45.0);
+        pCam->setPositionRotation( glm::vec3(300 , 0 ,0) , glm::vec3(0,0,0) );
+        pCam->setPerspective(true);
+        pCam->init();
+        group1->addChild(pCam);
+        //sceneMng->addChild(pCam);
 
         Light* pLight = new Light(group1,"Luz1");
         pLight->setDiffuse(Color::WHITE);
         pLight->setPositionRotation(glm::vec3(80,100,150), glm::vec3(0,0,0));
         //pLight->init();
 
-        sceneMng->origemDesenho((Coord*)pTrans);
-
         //Texture *pTex = new Texture("Texture-teste",dirDados + "/spacebox.png");
-        //Texture *pTex = new Texture("Texture-teste",dirDados + "/image1.jpg");
+        //Texture *pTex = new Texture("Texture-teste", TEX_SEQ::DIFFUSE, "./models/image1.jpg");
 		Material *pMat1 = new Material("Mat1");
 		pMat1->setAmbient( Color( 1.0f, 0.5f, 0.31f ) );
         pMat1->setDiffuse( Color( 1.0f, 0.5f, 0.31f ) );

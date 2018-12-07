3234b468f0e5308a67396dca59b95c181ab0c1d4
Eduardo Pagotto <eduardo.pagotto@newspace.com.br>
2018-12-07 12:22:49 -0200

    novo projeto de raycasting

---
+++ examples/simpleRayCasting/Game.hpp
@@ -0,0 +1,34 @@
+#ifndef __GAME_SIMPLE_RAYCASTING__HPP
+#define __GAME_SIMPLE_RAYCASTING__HPP
+
+#include "chimera/core/CanvasFB.hpp"
+#include "chimera/core/IGameClientEvents.hpp"
+
+class Game : public Chimera::IGameClientEvents {
+  public:
+    Game(Chimera::CanvasFB* _pCanvas);
+
+    virtual ~Game();
+    // Inherited via IGameClientEvents
+    virtual void start();
+    virtual void stop();
+    virtual void render();
+    virtual void keyCapture(SDL_Keycode tecla);
+    virtual void mouseButtonDownCapture(SDL_MouseButtonEvent mb);
+    virtual void mouseButtonUpCapture(SDL_MouseButtonEvent mb);
+    virtual void mouseMotionCapture(SDL_MouseMotionEvent mm);
+    virtual void joystickCapture(Chimera::JoystickManager& joy);
+    virtual void joystickStatus(Chimera::JoystickManager& joy);
+    virtual void newFPS(const unsigned int& fps);
+    virtual void userEvent(const SDL_Event& _event) override;
+    virtual void windowEvent(const SDL_WindowEvent& _event) override;
+    virtual bool paused() override;
+
+  private:
+    Chimera::CanvasFB* pCanvas;
+    int botaoIndex;
+    int estadoBotao;
+    bool isPaused;
+};
+
+#endif

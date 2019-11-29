7021742cdd509f54e02d794d306a56c0ef59c1d1
Edu Pagotto <edupagotto@gmail.com>
2019-11-29 20:56:00 -0300

    Light so node prop

--- chimera/node/Light.hpp
+++ chimera/node/Light.hpp
@@ -3,16 +3,16 @@
 
 #include "Node.hpp"
 #include "chimera/core/LightData.hpp"
+#include "chimera/node/NodeVisitor.hpp"
 
 namespace Chimera {
 
 class Light : public Node {
   public:
-    Light(Node* _parent, std::string _name);
-    virtual ~Light();
-    virtual void init() override;
-    virtual void accept(class NodeVisitor* v) override;
-
+    Light(Node* _parent, std::string _name) : Node(_parent, Kind::LIGHT, _name) {}
+    virtual ~Light() {}
+    virtual void init() override{};
+    virtual void accept(class NodeVisitor* v) override { v->visit(this); };
     LightData lightData;
 };
 } // namespace Chimera
--- chimera/src/node/Light.cpp
+++ chimera/src/node/Light.cpp
@@ -1,26 +1,11 @@
 #include "chimera/node/Light.hpp"
-#include "chimera/node/NodeVisitor.hpp"
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtx/quaternion.hpp>
-#include <vector>
+//#include "chimera/node/NodeVisitor.hpp"
 
 namespace Chimera {
 
-Light::Light(Node* _parent, std::string _name) : Node(_parent, Kind::LIGHT, _name) {}
+// Light::Light(Node* _parent, std::string _name) : Node(_parent, Kind::LIGHT, _name) {}
+// Light::~Light() {}
 
-Light::~Light() {}
-
-void Light::accept(NodeVisitor* v) { v->visit(this); }
-
-// void Light::apply(Shader* _pShader) {
-//     glm::vec3 val = glm::vec3(transform[3]); // pega posicao
-//     _pShader->setGlUniform3fv("light.position", 1, glm::value_ptr(val));
-//     _pShader->setGlUniform4fv("light.ambient", 1, glm::value_ptr(ambient));
-//     _pShader->setGlUniform4fv("light.diffuse", 1, glm::value_ptr(diffuse));
-//     _pShader->setGlUniform4fv("light.specular", 1, glm::value_ptr(specular));
-// }
-
-void Light::init() {}
+// void Light::accept(NodeVisitor* v) { v->visit(this); }
+// void Light::init() {}
 } // namespace Chimera

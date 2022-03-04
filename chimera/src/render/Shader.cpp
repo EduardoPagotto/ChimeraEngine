21063d021ddf3313940ba2927045aec2629db41f
Edu Pagotto <edupagotto@gmail.com>
2022-03-04 19:11:03 -0300

    limpeza

--- chimera/src/render/Shader.cpp
+++ chimera/src/render/Shader.cpp
@@ -5,18 +5,6 @@
 
 namespace Chimera {
 
-// --- shade internal func
-static GLenum shaderTypeFromString(const std::string type) {
-    if (type == "vertex")
-        return GL_VERTEX_SHADER;
-    if (type == "fragment" || type == "pixel")
-        return GL_FRAGMENT_SHADER;
-
-    SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Unknow shader type \"%s\"", type.c_str());
-
-    return 0;
-}
-
 static GLuint compileShader(const std::string& fileName, const std::string& shaderCode, uint16_t kindShade) {
 
     GLint Result = GL_FALSE;
@@ -81,61 +69,6 @@ static GLuint linkShader(const std::vector<GLuint>& vecShaderID) {
     }
 
     return ProgramID;
-}
-
-static std::unordered_map<GLenum, std::string> preProcess(const std::string source) {
-
-    std::unordered_map<GLenum, std::string> shaderSource;
-
-    const char* typeToken = "#type";
-    size_t typeTokenLenght = strlen(typeToken);
-    size_t pos = source.find(typeToken, 0);
-    while (pos != std::string::npos) {
-
-        size_t eol = source.find_first_of("\r\n", pos);
-        // log erro
-        if (eol == std::string::npos) {
-            SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Syntax error");
-            throw Exception(std::string("Syntax error ") + source);
-        }
-
-        size_t begin = pos + typeTokenLenght + 1;
-        std::string type = source.substr(begin, eol - begin);
-        GLenum t = shaderTypeFromString(type);
-
-        if (t == 0) {
-            SDL_LogError(SDL_LOG_CATEGORY_RENDER, "Invalid Shader");
-            throw Exception(std::string("Invalid Shader ") + source);
-        }
-
-        size_t nextLinePos = source.find_first_not_of("\r\n", eol);
-        pos = source.find(typeToken, nextLinePos);
-        shaderSource[t] = source.substr(nextLinePos, pos - (nextLinePos == std::string::npos ? source.size() - 1 : nextLinePos));
-    }
-
-    return shaderSource;
-}
-
-static GLuint shadeLoadProg(const std::string& filepath) {
-
-    std::vector<GLuint> vecShaderID;
-    std::string fileData = ShadeInclude::load(filepath); // utilReadFile(filepath);
-    if (fileData.size() == 0)
-        return 0;
-
-    auto shaderSources = preProcess(fileData); // split shders kind
-    for (auto& kv : shaderSources) {
-        GLenum type = kv.first;
-        const std::string source = kv.second;
-        vecShaderID.push_back(compileShader(filepath, source, type)); // compile chade
-    }
-
-    GLuint shaderId = linkShader(vecShaderID); // Link o programa
-
-    vecShaderID.clear();
-
-    SDL_LogDebug(SDL_LOG_CATEGORY_RENDER, "Shader %s id: %d", filepath.c_str(), (int)shaderId);
-    return shaderId;
 }
 
 //----

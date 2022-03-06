c1ee0317eb4ed89eec8e573b75ce49e011472102
Edu Pagotto <edupagotto@gmail.com>
2022-03-06 20:32:30 -0300

    parametro de debug para string NOVO projeto de loader independente e moderno

---
+++ samples/loadTest/main.cpp
@@ -0,0 +1,22 @@
+#include <SDL2/SDL.h>
+#include <cstdio>
+
+int main(int argn, char** argv) {
+    // using namespace Chimera;
+    //  try {
+    //  SDL_LogSetPriority(SDL_LOG_CATEGORY_SYSTEM, SDL_LOG_PRIORITY_DEBUG);
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_DEBUG);
+    SDL_Log("LoadTest Iniciado");
+
+    // Engine engine(new CanvasGL("models", 800, 600));
+    // Game* game = new Game(&engine);
+
+    // engine.pushState(game);
+    // engine.run();
+
+    // SDL_Log("Loop de Game encerrado!!!!");
+    // delete game;
+
+    SDL_Log("LoadTest finalizado com sucesso");
+    return 0;
+}

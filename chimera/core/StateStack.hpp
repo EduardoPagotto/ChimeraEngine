e30f0107ed8f45ae600bfe575b067088f05881fc
Edu Pagotto <edupagotto@gmail.com>
2022-06-15 19:38:29 -0300

    simplificacao

--- chimera/core/StateStack.hpp
+++ chimera/core/StateStack.hpp
@@ -1,23 +1,53 @@
 #pragma once
 #include "IStateMachine.hpp"
 #include <vector>
+#include <algorithm>
 
 namespace Chimera {
 
 class StateStack {
   public:
-    StateStack();
-    ~StateStack();
+    StateStack() { stateInsert = states.begin(); }
+    virtual ~StateStack() { states.clear(); }
     inline void clear() { states.clear(); }
 
-    void pushState(IStateMachine* state);
-    void pushOverlay(IStateMachine* overlay);
-    void popState(IStateMachine* state);
-    void popOverlay(IStateMachine* overlay);
+    inline void pushState(IStateMachine* state) {
+        stateInsert = states.emplace(stateInsert, state);
+        state->onAttach();
+    }
 
-    IStateMachine* getState(const std::string& name) const;
-    std::vector<IStateMachine*>::iterator begin() { return states.begin(); }
-    std::vector<IStateMachine*>::iterator end() { return states.end(); }
+    inline void pushOverlay(IStateMachine* overlay) {
+        states.emplace_back(overlay);
+        overlay->onAttach();
+    }
+
+    inline void popState(IStateMachine* state) {
+        auto it = std::find(states.begin(), states.end(), state);
+        if (it != states.end()) {
+            states.erase(it);
+            stateInsert--;
+        }
+        state->onDeatach();
+    }
+
+    inline void popOverlay(IStateMachine* overlay) {
+        auto it = std::find(states.begin(), states.end(), overlay);
+        if (it != states.end())
+            states.erase(it);
+
+        overlay->onDeatach();
+    }
+
+    inline IStateMachine* getState(const std::string& name) const {
+        for (IStateMachine* state : states) {
+            if (state->getName() == name)
+                return state;
+        }
+        return nullptr;
+    }
+
+    inline std::vector<IStateMachine*>::iterator begin() { return states.begin(); }
+    inline std::vector<IStateMachine*>::iterator end() { return states.end(); }
 
   private:
     std::vector<IStateMachine*> states;

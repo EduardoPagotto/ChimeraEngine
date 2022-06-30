9347815f63afdfab58f0cf6e3694f2baf6a64ec3
Edu Pagotto <edupagotto@gmail.com>
2022-06-30 15:36:38 -0300

    Classe de hierarquia com componentes do ECS

--- chimera/core/Registry.hpp
+++ chimera/core/Registry.hpp
@@ -18,7 +18,8 @@ class Registry {
     Entity findEntityId(const std::string& id);
     Entity findEntitySerial(const uint32_t& serial);
 
-    template <typename T> T& findComponent(const std::string& tagName) {
+    template <typename T>
+    T& findComponent(const std::string& tagName) {
         auto view = eRegistry.view<T>();
         for (auto ent : view) {
             TagComponent& ee = eRegistry.get<TagComponent>(ent);
@@ -39,20 +40,80 @@ class Entity {
     Entity() = default;
     Entity(entt::entity handle, Registry* reg) : handle(handle), reg(reg) {}
     Entity(const Entity& other) : handle(other.handle), reg(other.reg) {}
-    template <typename T> bool hasComponent() { return reg->eRegistry.all_of<T>(handle); }
-    template <typename T, typename... Args> T& addComponent(Args&&... args) {
+    template <typename T>
+    bool hasComponent() {
+        return reg->eRegistry.all_of<T>(handle);
+    }
+    template <typename T, typename... Args>
+    T& addComponent(Args&&... args) {
         return reg->eRegistry.emplace<T>(handle, std::forward<Args>(args)...);
     }
-    template <typename T> T& getComponent() { return reg->eRegistry.get<T>(handle); }
-    template <typename T> void removeComponent() { reg->eRegistry.remove<T>(handle); }
+    template <typename T>
+    T& getComponent() {
+        return reg->eRegistry.get<T>(handle);
+    }
+    template <typename T>
+    void removeComponent() {
+        reg->eRegistry.remove<T>(handle);
+    }
     operator bool() const { return handle != entt::null; }
     operator uint32_t() const { return (uint32_t)handle; }
     operator entt::entity() const { return handle; }
     bool operator==(const Entity& other) const { return handle == other.handle && reg == other.reg; }
     bool operator!=(const Entity& other) const { return !(*this == other); }
+    Registry* getRegistry() const { return reg; }
 
   private:
     entt::entity handle{entt::null};
     Registry* reg = nullptr;
 };
+
+struct Hierarchie {
+    Hierarchie() = default;
+    std::size_t children{};
+    Entity local, first, prev, next, parent;
+};
+
+class Relationship {
+  public:
+    Relationship(Hierarchie* h) : hierarchie(h) {}
+    Relationship(const Relationship& o) : hierarchie(o.hierarchie) {}
+    Relationship(Registry& r, const std::string& tag, const std::string& id) {
+        Entity e = r.createEntity(tag, id);
+        Relationship& novo = e.addComponent<Relationship>(new Hierarchie());
+        novo.get()->local = e;
+        this->hierarchie = novo.get();
+    }
+    Hierarchie* get() const { return hierarchie; }
+
+    Relationship addChild(const std::string& tag, const std::string& id) {
+        Relationship novo(*hierarchie->local.getRegistry(), tag, id);
+        novo.get()->parent = hierarchie->local;
+
+        if (hierarchie->children == 0) {
+            hierarchie->first = novo.get()->local;
+        } else {
+            Relationship& last = hierarchie->first.getComponent<Relationship>();
+            while (last.get()->next != Entity())
+                last = last.get()->next.getComponent<Relationship>();
+
+            last.get()->next = novo.get()->local;
+            novo.get()->prev = last.get()->local;
+        }
+
+        hierarchie->children++;
+        return novo;
+    }
+
+  private:
+    Hierarchie* hierarchie = nullptr;
+};
+// ref: https://skypjack.github.io/2019-06-25-ecs-baf-part-4/
+// Engine engine;
+// Relationship obj0(engine.getRegistry(), "root", "root");
+// Relationship child0 = obj0.addChild("child00", "child00");
+// Relationship child1 = obj0.addChild("child01", "child01");
+// Relationship child2 = obj0.addChild("child02", "child02");
+// Relationship child3 = child2.addChild("child03", "child03");
+// Relationship child4 = child2.addChild("child04", "child04");
 } // namespace Chimera

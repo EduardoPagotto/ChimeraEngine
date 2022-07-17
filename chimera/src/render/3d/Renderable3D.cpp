97c4d93a128c65fc4bf5e8bc39072c980ba5d77f
Eduardo Pagotto <edupagotto@gmail.com>
2022-07-17 17:42:48 -0300

    Limpeza

--- chimera/src/render/3d/Renderable3D.cpp
+++ chimera/src/render/3d/Renderable3D.cpp
@@ -1,9 +1,6 @@
 #include "chimera/render/3d/Renderable3D.hpp"
-#include "chimera/core/OpenGLDefs.hpp"
-#include "chimera/core/visible/RenderCommand.hpp"
-#include "chimera/render/3d/IRenderer3d.hpp"
-#include "chimera/render/3d/RenderableIBO.hpp"
-#include "chimera/render/VertexData.hpp"
+#include "chimera/core/buffer/VertexArray.hpp"
+#include <SDL2/SDL.h>
 
 namespace Chimera {
 
@@ -18,5 +15,4 @@ void Renderable3D::draw(const bool& logData) {
     if (logData)
         SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, "Renderable3D draw");
 }
-
 } // namespace Chimera

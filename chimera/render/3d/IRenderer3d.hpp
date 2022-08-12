6b062b7ef0f2c9827ccf8f9e94c81f252c017b9f
Edu Pagotto <edupagotto@gmail.com>
2022-08-12 13:00:18 -0300

    Rendered3d nao e mais interface

--- chimera/render/3d/IRenderer3d.hpp
+++ chimera/render/3d/IRenderer3d.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "chimera/core/TransformationStack.hpp"
+#include "chimera/core/visible/ICamera.hpp"
 #include "chimera/core/visible/UValue.hpp"
 #include <vector>
 namespace Chimera {
@@ -10,8 +12,14 @@ class IRenderer3d {
     virtual void flush() = 0;
     virtual MapUniform& uboQueue() = 0;
     virtual std::vector<class Texture*>& texQueue() = 0;
-    virtual class TransformationStack& getStack() = 0;
-    virtual class Camera* getCamera() const = 0;
-    virtual class ViewProjection* getEyeView() const = 0;
+
+    inline Camera* getCamera() const { return camera; }
+    inline ViewProjection* getViewProjection() const { return vpo; }
+    inline TransformationStack& getStack() { return stack; };
+
+  protected:
+    Camera* camera;
+    ViewProjection* vpo;
+    TransformationStack stack; // TODO: implementar a hierarquia de modelos direta (sem fisica)
 };
 } // namespace Chimera
--- chimera/render/3d/Renderer3d.hpp
+++ chimera/render/3d/Renderer3d.hpp
@@ -1,9 +1,7 @@
 #pragma once
 #include "IRenderer3d.hpp"
-#include "chimera/core/TransformationStack.hpp"
 #include "chimera/core/space/Frustum.hpp"
 #include "chimera/core/space/Octree.hpp"
-#include "chimera/core/visible/ICamera.hpp"
 #include "chimera/core/visible/RenderCommand.hpp"
 #include "chimera/render/3d/Renderable3D.hpp"
 #include <cstdint>
@@ -19,22 +17,17 @@ class Renderer3d : public IRenderer3d {
     virtual bool submit(const RenderCommand& command, IRenderable3d* renderable) override;
     virtual void end() override;
     virtual void flush() override;
-    virtual inline TransformationStack& getStack() override { return stack; };
+
     virtual inline MapUniform& uboQueue() override { return uniformsQueue; }
     virtual inline std::vector<Texture*>& texQueue() override { return textureQueue; }
-    virtual inline Camera* getCamera() const override { return camera; }
-    virtual inline ViewProjection* getEyeView() const override { return vpo; }
 
   private:
-    Camera* camera;
-    ViewProjection* vpo;
     std::vector<std::tuple<RenderCommand, Renderable3D*>> renderQueue;
     std::vector<Texture*> textureQueue;
     MapUniform uniformsQueue;
     Frustum frustum;
     uint32_t totIBO, totFaces;
     bool logData;
-    TransformationStack stack; // TODO: implementar a hierarquia de modelos direta (sem fisica)
     Octree* octree;
 };
 } // namespace Chimera

7bce05d48e716be7f4107156627b5ef95bf7505c
Edu Pagotto <edupagotto@gmail.com>
2022-08-20 13:54:47 -0300

    ajuste

--- chimera/src/render/3d/Renderer3d.cpp
+++ chimera/src/render/3d/Renderer3d.cpp
@@ -71,51 +71,48 @@ void Renderer3d::flush() {
 
     while (!qRenderableIndexes.empty()) {
         auto& r = vRenderable[qRenderableIndexes.front()];
+        if (r->getVao() != pLastVao) { // Diferente  do anterior
+            if (pLastVao != nullptr) { // desvincula o anterior
+                pLastVao->unbind();
+            }
 
-        if (r->getVao() != nullptr) {      // Possui um novo modelo
-            if (r->getVao() != pLastVao) { // Diferente  do anterior
-                if (pLastVao != nullptr) { // desvincula o anterior
-                    pLastVao->unbind();
-                }
+            const RenderCommand& command = vRenderCommand[r->getIndexAuxCommand()];
+            r->getVao()->bind(); // vincula novo modelo
+            pLastVao = r->getVao();
 
-                const RenderCommand& command = vRenderCommand[r->getIndexAuxCommand()];
-                r->getVao()->bind(); // vincula novo modelo
-                pLastVao = r->getVao();
-
-                if (activeShader.isInvalid()) { // primeira passada
-                    activeShader = command.shader;
-                    glUseProgram(activeShader.getID());
-                } else {
-                    // demais passadas
-                    if (activeShader != command.shader) {  // se diferente
-                        if (!command.shader.isInvalid()) { // se valido trocar
-                            glUseProgram(0);
-                            activeShader = command.shader;
-                            glUseProgram(activeShader.getID());
-                        }
+            if (activeShader.isInvalid()) { // primeira passada
+                activeShader = command.shader;
+                glUseProgram(activeShader.getID());
+            } else {
+                // demais passadas
+                if (activeShader != command.shader) {  // se diferente
+                    if (!command.shader.isInvalid()) { // se valido trocar
+                        glUseProgram(0);
+                        activeShader = command.shader;
+                        glUseProgram(activeShader.getID());
                     }
                 }
-
-                // generic bind in each draw call camera, light, etc
-                for (const auto& kv : uniformsQueue)
-                    activeShader.setUniformU(kv.first.c_str(), kv.second);
-
-                // bind dos uniforms from model
-                for (const auto& kv : command.uniforms)
-                    activeShader.setUniformU(kv.first.c_str(), kv.second);
-
-                // libera textura antes de passar as novas
-                if (command.vTex.size() == 0)
-                    Texture::unbind(0);
-
-                // bind de texturas
-                for (uint8_t i = 0; i < command.vTex.size(); i++)
-                    command.vTex[i]->bind(i);
-
-                // bind de texturas globais
-                for (uint8_t i = 0; i < textureQueue.size(); i++)
-                    textureQueue[i]->bind(command.vTex.size() + i);
             }
+
+            // generic bind in each draw call camera, light, etc
+            for (const auto& kv : uniformsQueue)
+                activeShader.setUniformU(kv.first.c_str(), kv.second);
+
+            // bind dos uniforms from model
+            for (const auto& kv : command.uniforms)
+                activeShader.setUniformU(kv.first.c_str(), kv.second);
+
+            // libera textura antes de passar as novas
+            if (command.vTex.size() == 0)
+                Texture::unbind(0);
+
+            // bind de texturas
+            for (uint8_t i = 0; i < command.vTex.size(); i++)
+                command.vTex[i]->bind(i);
+
+            // bind de texturas globais
+            for (uint8_t i = 0; i < textureQueue.size(); i++)
+                textureQueue[i]->bind(command.vTex.size() + i);
         }
 
         r->draw(logData); // aqui

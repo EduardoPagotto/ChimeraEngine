69f9f979055f56e2f58dcd3a1c0f4e50bca807e6
Edu Pagotto <edupagotto@gmail.com>
2023-11-26 20:40:16 -0300

    removidos cpps para headers

--- chimera/core/device/Keyboard.hpp
+++ chimera/core/device/Keyboard.hpp
@@ -7,11 +7,38 @@ namespace Chimera {
 
 class Keyboard {
   public:
-    static void setDown(const SDL_KeyboardEvent& event);
-    static void setUp(const SDL_KeyboardEvent& event);
-    static bool isPressed(const SDL_Keycode& key);
-    static bool isModPressed(const SDL_Keymod& keyMod);
-    static bool getEvent(const SDL_Event& event);
+    static void setDown(const SDL_KeyboardEvent& event) {
+        mapKey[event.keysym.sym] = SDL_PRESSED;
+        mod = event.keysym.mod;
+    }
+
+    static void setUp(const SDL_KeyboardEvent& event) {
+        mapKey[event.keysym.sym] = SDL_RELEASED;
+        mod = event.keysym.mod;
+    }
+
+    static bool isPressed(const SDL_Keycode& key) {
+        if (auto it = mapKey.find(key); it != mapKey.end())
+            return mapKey[key] == SDL_PRESSED;
+
+        return false;
+    }
+
+    static bool isModPressed(const SDL_Keymod& keyMod) { return (keyMod & mod); }
+
+    static bool getEvent(const SDL_Event& event) {
+
+        switch (event.type) {
+            case SDL_KEYDOWN:
+                Keyboard::setDown(event.key);
+                break;
+            case SDL_KEYUP:
+                Keyboard::setUp(event.key);
+                break;
+        }
+
+        return false;
+    }
 
   private:
     inline static std::map<SDL_Keycode, uint8_t> mapKey;
--- chimera/core/device/MouseDevice.hpp
+++ chimera/core/device/MouseDevice.hpp
@@ -8,13 +8,50 @@ namespace Chimera {
 
 class MouseDevice {
   public:
-    static uint8_t getButtonState(const uint8_t& indice);
-    static const glm::ivec2 getMove();
-    static const glm::ivec2 getMoveRel();
-    static void updateBt(const SDL_MouseButtonEvent& bt);
-    static void updateMv(const SDL_MouseMotionEvent& mv);
-    static void updateWl(const SDL_MouseWheelEvent& mwe);
-    static bool getEvent(const SDL_Event& event);
+    static uint8_t getButtonState(const uint8_t& indice) {
+        auto button_iter = MouseDevice::buttonState.find(indice);
+        if (button_iter != buttonState.end())
+            return button_iter->second;
+
+        return SDL_RELEASED;
+    }
+
+    static const glm::ivec2 getMove() { return pos; }
+
+    static const glm::ivec2 getMoveRel() {
+        if (flag1 != flag2) {
+            flag1 = flag2;
+            return rel;
+        }
+        return glm::ivec2(0);
+    }
+
+    static void updateBt(const SDL_MouseButtonEvent& bt) { MouseDevice::buttonState[bt.button] = bt.state; }
+
+    static void updateMv(const SDL_MouseMotionEvent& mv) {
+        MouseDevice::pos = glm::ivec2(mv.x, mv.y);
+        MouseDevice::rel = glm::ivec2(mv.xrel, mv.yrel);
+        flag1++;
+    }
+
+    static void updateWl(const SDL_MouseWheelEvent& mwe) { MouseDevice::wheel = glm::ivec2(mwe.x, mwe.y); }
+
+    static bool getEvent(const SDL_Event& event) {
+        switch (event.type) {
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+                MouseDevice::updateBt(event.button);
+                break;
+            case SDL_MOUSEMOTION:
+                MouseDevice::updateMv(event.motion);
+                break;
+            case SDL_MOUSEWHEEL:
+                MouseDevice::updateWl(event.wheel);
+                break;
+        }
+
+        return false;
+    }
 
   private:
     inline static std::map<uint8_t, uint8_t> buttonState;

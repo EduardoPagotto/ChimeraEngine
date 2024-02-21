3180d2120ae8158276e255cbd2837624f4e318c9
Edu Pagotto <edupagotto@gmail.com>
2024-02-21 17:44:51 -0300

    removido todos os cpp dos collada

--- chimera/core/collada/ColladaHeightMap.hpp
+++ chimera/core/collada/ColladaHeightMap.hpp
@@ -1,14 +1,33 @@
 #pragma once
 #include "../Registry.hpp"
 #include "Collada.hpp"
+#include "chimera/core/LoadHeightMap.hpp"
 
 namespace Chimera {
 class ColladaHeightMap : public Collada {
   public:
     ColladaHeightMap(ColladaDom& dom, const std::string& url) : Collada(dom, url){};
-    virtual ~ColladaHeightMap();
-    void create(const std::string& id, const std::string& name, Entity& entity, pugi::xml_node geo);
 
-  private:
+    virtual ~ColladaHeightMap() {}
+
+    void create(const std::string& id, const std::string& name, Entity& entity, pugi::xml_node geo) {
+
+        MeshComponent& mc = entity.addComponent<MeshComponent>();
+        mc.tag.id = id;
+        mc.tag.tag = name;
+        mc.tag.serial = Collada::getNewSerial();
+
+        std::string target = geo.attribute("target").value();
+        uint32_t square_x = static_cast<uint32_t>(std::stoul(geo.attribute("square_x").value()));
+        uint32_t square_z = static_cast<uint32_t>(std::stoul(geo.attribute("square_z").value()));
+        glm::vec3 size = textToVec3(geo.next_sibling("size").text().as_string());
+
+        LoadHeightMap loader(square_x, square_z);
+
+        loader.getMesh(target, *mc.mesh, size);
+
+        loader.split(mc.mesh->iFace, mc.vTrisIndex);
+        mc.type = getMeshTypeFromString(geo.attribute("partition").value());
+    }
 };
 } // namespace Chimera
--- chimera/core/collada/ColladaLight.hpp
+++ chimera/core/collada/ColladaLight.hpp
@@ -1,15 +1,38 @@
 #pragma once
 #include "../Registry.hpp"
 #include "Collada.hpp"
+#include "chimera/core/Light.hpp"
 
 namespace Chimera {
 class ColladaLight : public Collada {
   public:
     ColladaLight(ColladaDom& dom, const std::string& url) : Collada(dom, url){};
-    virtual ~ColladaLight();
-    void create(Entity& entity, pugi::xml_node nodeLight);
 
-  private:
+    virtual ~ColladaLight() {}
+
+    void create(Entity& entity, pugi::xml_node nodeLight) { // FIXME: preciso mesmo da entidade ???
+        LightComponent& lc = entity.addComponent<LightComponent>();
+        lc.tag.id = nodeLight.attribute("id").value();
+        lc.tag.tag = nodeLight.attribute("name").value();
+        lc.tag.serial = Collada::getNewSerial();
+
+        pugi::xml_node tec = nodeLight.child("technique_common");
+        for (pugi::xml_node lTec = tec.first_child(); lTec; lTec = lTec.next_sibling()) {
+
+            if (std::string name = lTec.name(); name == "point") {
+                std::string color = lTec.child("color").text().as_string();
+                lc.light->setDiffuse(textToVec4(color));
+                lc.light->setAmbient(glm::vec4(0.9f, 0.9f, 0.9f, 1.0f)); // FIXME: remover depois
+                lc.light->setType(LightType::POSITIONAL);
+
+            } else if (name == "directional") {
+                // TODO: implementar
+                std::string color = lTec.child("color").text().as_string();
+                lc.light->setDiffuse(textToVec4(color));
+                lc.light->setAmbient(glm::vec4(0.9f, 0.9f, 0.9f, 1.0f)); // FIXME: remover depois
+                lc.light->setType(LightType::DIRECTIONAL);
+            }
+        }
+    }
 };
-
 } // namespace Chimera

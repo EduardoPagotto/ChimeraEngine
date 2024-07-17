7e9d8e54e2e20a7b61f2bb17f4d9c419c46c0310
Edu Pagotto <edupagotto@gmail.com>
2024-07-16 23:35:33 -0300

    FontManager removido cpp

--- chimera/core/visible/FontManager.hpp
+++ chimera/core/visible/FontManager.hpp
@@ -6,14 +6,33 @@ namespace Chimera {
 
 class FontManager {
   public:
-    static void add(FontAtlas* font);
-    static void clean();
-    static FontAtlas* get(const std::string& name);
+    static void add(FontAtlas* font) { fonts.push_back(font); }
+
+    static void clean() {
+        for (int i = 0; i < fonts.size(); i++)
+            delete fonts[i];
+
+        fonts.clear();
+    }
+    static FontAtlas* get(const std::string& name) {
+        // TODO: modificar mapara mapa
+
+        for (FontAtlas* font : fonts) {
+            if (font->getName() == name)
+                return font;
+        }
+
+        // FIXME: colocar fonte default se elea nao carregar
+        return nullptr;
+    }
+
     static FontAtlas* get() { return fonts[0]; }
 
   private:
     FontManager() {}
+
     ~FontManager() {}
+
     inline static std::vector<FontAtlas*> fonts;
 };
 } // namespace Chimera

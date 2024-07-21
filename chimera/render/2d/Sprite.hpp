60da9ffa565c77895840afa23785ec45ef4b1586
Edu Pagotto <edupagotto@gmail.com>
2024-07-21 19:40:36 -0300

    limpeza 2d

--- chimera/render/2d/Sprite.hpp
+++ chimera/render/2d/Sprite.hpp
@@ -5,8 +5,12 @@ namespace Chimera {
 
 class Sprite : public Renderable2D {
   public:
-    Sprite(float x, float y, float width, float height, glm::vec4 color);
-    Sprite(float x, float y, float width, float height, std::shared_ptr<Texture> texture);
+    Sprite(float x, float y, float width, float height, glm::vec4 color)
+        : Renderable2D(glm::vec3(x, y, 0), glm::vec2(width, height), color) {}
+
+    Sprite(float x, float y, float width, float height, std::shared_ptr<Texture> texture)
+        : Renderable2D(glm::vec3(x, y, 0), glm::vec2(width, height), texture) {}
+
     virtual ~Sprite() {}
 };
 } // namespace Chimera

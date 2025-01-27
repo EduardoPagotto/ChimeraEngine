f11d67ba6e5c8e5be6991b805155e9230f603ff9
Edu Pagotto <edupagotto@gmail.com>
2025-01-27 19:16:14 -0300

    Baguncar para depois organizar

--- chimera/core/Engine.hpp
+++ chimera/core/Engine.hpp
@@ -1,27 +1,19 @@
 #pragma once
 #include "StateStack.hpp"
 #include "Timer.hpp"
-#include "chimera/core/Registry.hpp"
-#include "chimera/core/ViewProjection.hpp"
-#include "chimera/core/device/interfaces.hpp"
+#include "interfaces.hpp"
 
-namespace Chimera {
+namespace me {
 class Engine {
-  public:
-    Engine(std::shared_ptr<ServiceLocator> serviceLoc);
-    virtual ~Engine();
-    void run();
-    // ICanva* getCanvas() const { return canvas.get(); }
-    StateStack& getStack() { return stack; }
-
-  private:
-    std::shared_ptr<ServiceLocator> serviceLoc;
-    std::shared_ptr<ICanva> canvas;
-    std::shared_ptr<IViewProjection> vp;
-    std::shared_ptr<Registry> registry;
-
     uint32_t fps = 140;
     Timer timerFPS;
     StateStack stack;
+    std::shared_ptr<ICanva> canvas;
+
+  public:
+    Engine();
+    ~Engine();
+    void run();
+    StateStack& getStack() { return stack; }
 };
-} // namespace Chimera
+} // namespace me

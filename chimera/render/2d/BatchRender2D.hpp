6cac32963651de8b85a18f7c28442f464ffbdc38
Edu Pagotto <edupagotto@gmail.com>
2025-05-28 20:46:56 -0300

    BatchRender2D shared

--- chimera/render/2d/BatchRender2D.hpp
+++ chimera/render/2d/BatchRender2D.hpp
@@ -7,44 +7,55 @@
 
 namespace ce {
 
-#define RENDERER_MAX_SPRITES 60000
-#define RENDERER_SPRITE_SIZE 4
-#define RENDERER_BUFFER_SIZE RENDERER_SPRITE_SIZE* RENDERER_MAX_SPRITES
+#define RENDERER_MAX_SPRITES  60000
+#define RENDERER_SPRITE_SIZE  4
+#define RENDERER_BUFFER_SIZE  RENDERER_SPRITE_SIZE* RENDERER_MAX_SPRITES
 #define RENDERER_INDICES_SIZE RENDERER_MAX_SPRITES * 6
-#define RENDERER_MAX_TEXTURE 32
+#define RENDERER_MAX_TEXTURE  32
 
-struct VertexDataSimple {
-    glm::vec3 point; // 3 * 4 = 12 (0 - 11)
-    glm::vec2 uv;    // 2 * 4 = 8  (12 - 19)
-    float tid;       // 1 * 4 = 4  (20 - 23)
-    glm::vec4 color; // 4 * 4 = 16 (24 - 39)
-};
+    struct VertexDataSimple {
+        glm::vec3 point; // 3 * 4 = 12 (0 - 11)
+        glm::vec2 uv;    // 2 * 4 = 8  (12 - 19)
+        float tid;       // 1 * 4 = 4  (20 - 23)
+        glm::vec4 color; // 4 * 4 = 16 (24 - 39)
+    };
 
-class BatchRender2D : public IRenderer2D {
-  public:
-    BatchRender2D();
-    virtual ~BatchRender2D();
+    class BatchRender2D : public IRenderer2D {
 
-    void init();
-    virtual void begin(Camera* camera) override;
-    virtual void submit(IRenderable2D* renderable) override;
-    virtual void end() override;
-    virtual void flush() override;
-    virtual void drawString(std::shared_ptr<Font> font, const std::string& text, const glm::vec3& pos, const glm::vec4& color) override;
-    inline virtual TransformationStack& getStack() override { return stack; };
-    inline virtual void setCommandRender(struct RenderCommand* command) override { renderComando = command; }
+      private:
+        TransformationStack stack;
+        std::shared_ptr<IndexBuffer> ibo;
+        std::shared_ptr<VertexArray> pVao;
+        std::shared_ptr<VertexBuffer> pVbo;
+        GLsizei indexCount;
+        VertexDataSimple* buffer;
+        RenderCommand* renderComando;
+        std::vector<std::shared_ptr<Texture>> textures;
+        Camera* camera;
 
-  private:
-    float submitTexture(std::shared_ptr<Texture> texture);
+      public:
+        BatchRender2D();
 
-    TransformationStack stack;
-    IndexBuffer* ibo;
-    VertexArray* pVao;
-    VertexBuffer* pVbo;
-    GLsizei indexCount;
-    VertexDataSimple* buffer;
-    RenderCommand* renderComando;
-    std::vector<std::shared_ptr<Texture>> textures;
-    Camera* camera;
-};
+        virtual ~BatchRender2D();
+
+        void init();
+
+        virtual void begin(Camera* camera) override;
+
+        virtual void submit(IRenderable2D* renderable) override;
+
+        virtual void end() override;
+
+        virtual void flush() override;
+
+        virtual void drawString(std::shared_ptr<Font> font, const std::string& text, const glm::vec3& pos,
+                                const glm::vec4& color) override;
+
+        inline virtual TransformationStack& getStack() override { return stack; };
+
+        inline virtual void setCommandRender(struct RenderCommand* command) override { renderComando = command; }
+
+      private:
+        float submitTexture(std::shared_ptr<Texture> texture);
+    };
 } // namespace ce
--- chimera/render/2d/IRenderer2D.hpp
+++ chimera/render/2d/IRenderer2D.hpp
@@ -4,15 +4,23 @@
 #include <string>
 
 namespace ce {
+
     class IRenderer2D {
+
       public:
         virtual void begin(class Camera* camera) = 0;
+
         virtual void submit(class IRenderable2D* renderable) = 0;
+
         virtual void end() = 0;
+
         virtual void flush() = 0;
+
         virtual TransformationStack& getStack() = 0;
+
         virtual void drawString(std::shared_ptr<Font> font, const std::string& text, const glm::vec3& pos,
                                 const glm::vec4& color) = 0;
+
         virtual void setCommandRender(struct RenderCommand* command) = 0;
     };
 } // namespace ce

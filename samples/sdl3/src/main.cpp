d8edc25c3ba0b07619765a6d1f52d2b00f91e282
Edu Pagotto <edupagotto@gmail.com>
2026-08-01 17:05:36 -0300

    AsseManagem em cevk_engine

--- samples/sdl3/src/main.cpp
+++ samples/sdl3/src/main.cpp
@@ -1,47 +1,9 @@
-#include "AssetManager.hpp"
 #include "VulkanRenderer.hpp"
-#include "cevk/TextureBindless.hpp"
-#include "cevk/VulkanTexture.hpp"
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_log.h>
 #include <glm/ext/matrix_transform.hpp>
 #include <memory>
 #include <string>
-
-void teste(std::shared_ptr<ce::VulkanContext> ctx) {
-
-    using namespace ce;
-
-    // 1. Cria o Asset Manager injetando o contexto Vulkan
-    AssetManager assetManager(ctx);
-    TextureBindless texBindless(ctx);
-
-    // Sampler sampler(ctx->logical);
-
-    // std::string fileLoc = "./assets/textures/" + filePath;
-
-    // 2. Carrega textura no Escopo Global
-    auto logo = assetManager.load<VulkanTexture>("logo_jogos", "./assets/textures/grid1.png");
-
-    texBindless.allocTexture(logo);
-
-    {
-        // 3. Entra em uma nova fase (Cria novo escopo)
-        assetManager.pushScope();
-
-        // Carrega textura específica desta fase
-        auto bg = assetManager.load<VulkanTexture>("background", "./assets/textures/grid2.png");
-
-        texBindless.allocTexture(bg);
-
-        // Se pedir a "logo_jogos" aqui, ele busca e encontra no escopo Global pai
-        auto logoMenu = assetManager.load<VulkanTexture>("logo_jogos", "./assets/textures/grid3.png");
-
-    } // 5. Sai da fase (PopScope deve ser chamado manualmente ou via RAII)
-    assetManager.popScope();
-    // Neste ponto, "fase1_bg.png" foi destruído do Vulkan automaticamente.
-    // "logo.png" continua vivo no escopo global.
-}
 
 int main() {
 
@@ -63,8 +25,6 @@ int main() {
         float lastTime = 0;
 
         int helicopter = vulkanRenderer.createMeshModel("./assets/models/Seahawk.obj");
-
-        // teste(context);
 
         for (bool running = true; running;) {
             SDL_Event event;
--- samples/sdl31/src/main.cpp
+++ samples/sdl31/src/main.cpp
@@ -1,8 +1,45 @@
 #include "Game.hpp"
+#include "cevk/TextureBindless.hpp"
+#include "cevk_engine/AssetManager.hpp"
 #include "cevk_engine/ScreenVK.hpp"
 #include "cevk_infra/Engine.hpp"
 #include <SDL3/SDL.h>
 #include <memory>
+
+void teste(std::shared_ptr<ce::VulkanContext> ctx) {
+
+    using namespace ce;
+
+    // 1. Cria o Asset Manager injetando o contexto Vulkan
+    AssetManager assetManager(ctx);
+    TextureBindless texBindless(ctx);
+
+    // Sampler sampler(ctx->logical);
+
+    // std::string fileLoc = "./assets/textures/" + filePath;
+
+    // 2. Carrega textura no Escopo Global
+    auto logo = assetManager.load<VulkanTexture>("logo_jogos", "./assets/textures/grid1.png");
+
+    texBindless.allocTexture(logo);
+
+    {
+        // 3. Entra em uma nova fase (Cria novo escopo)
+        assetManager.pushScope();
+
+        // Carrega textura específica desta fase
+        auto bg = assetManager.load<VulkanTexture>("background", "./assets/textures/grid2.png");
+
+        texBindless.allocTexture(bg);
+
+        // Se pedir a "logo_jogos" aqui, ele busca e encontra no escopo Global pai
+        auto logoMenu = assetManager.load<VulkanTexture>("logo_jogos", "./assets/textures/grid3.png");
+
+    } // 5. Sai da fase (PopScope deve ser chamado manualmente ou via RAII)
+    assetManager.popScope();
+    // Neste ponto, "fase1_bg.png" foi destruído do Vulkan automaticamente.
+    // "logo.png" continua vivo no escopo global.
+}
 
 int main(int argc, char* argv[]) {
 
@@ -21,6 +58,8 @@ int main(int argc, char* argv[]) {
 
         ce::Engine engine(scr);
         std::shared_ptr<ce::IStateMachine> game = std::make_shared<Game>(ctx, scr);
+
+        teste(ctx);
 
         engine.getStack().pushState(game);
         engine.run();
